c400abb4a74505a18d1b49ad7c446fe0866b1001
ihrapsa <40600040+ihrapsa@users.noreply.github.com>
2021-07-08 15:22:07 +0300

    Update filebrouser.lua to english

--- luci-app-filebrowser/luasrc/model/cbi/filebrowser.lua
+++ luci-app-filebrowser/luasrc/model/cbi/filebrowser.lua
@@ -1,4 +1,4 @@
-m = Map("filebrowser", translate("文件管理器"), translate("FileBrowser是一个基于Go的在线文件管理器，助您方便的管理设备上的文件。"))
+m = Map("filebrowser", translate("FileBrowser"), translate("FileBrowser is an online file manager based on Go that helps you manage files on your device conveniently."))
 
 m:section(SimpleSection).template  = "filebrowser/filebrowser_status"
 
@@ -6,37 +6,38 @@ s = m:section(TypedSection, "filebrowser")
 s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enabled", translate("启用"))
+enable = s:option(Flag, "enabled", translate("Enable"))
 enable.rmempty = false
 
-o = s:option(ListValue, "addr_type", translate("监听地址"))
-o:value("local", translate("监听本机地址"))
-o:value("lan", translate("监听局域网地址"))
-o:value("wan", translate("监听全部地址"))
+o = s:option(ListValue, "addr_type", translate("Listening address"))
+o:value("local", translate("Listen to local address"))
+o:value("lan", translate("Listen to LAN addresses"))
+o:value("wan", translate("Listen to all addresses"))
 o.default = "lan"
 o.rmempty = false
 
-o = s:option(Value, "port", translate("监听端口"))
+o = s:option(Value, "port", translate("Listening port"))
 o.placeholder = 8989
 o.default     = 8989
 o.datatype    = "port"
 o.rmempty     = false
 
-o = s:option(Value, "root_dir", translate("开放目录"))
+o = s:option(Value, "root_dir", translate("Root path"))
+o.description = translate("Point to a path to access your files in the web interface")
 o.placeholder = "/"
 o.default     = "/"
 o.rmempty     = false
 
-o = s:option(Value, "db_dir", translate("数据库目录"))
+o = s:option(Value, "db_dir", translate("Database directory"))
 o.placeholder = "/etc"
 o.default     = "/etc"
 o.rmempty     = false
-o.description = translate("普通用户请勿随意更改")
+o.description = translate("Users should not change this")
 
-o = s:option(Value, "db_name", translate("数据库名"))
+o = s:option(Value, "db_name", translate("Database filename"))
 o.placeholder = "filebrowser.db"
 o.default     = "filebrowser.db"
 o.rmempty     = false
-o.description = translate("普通用户请勿随意更改")
+o.description = translate("Users should not change this")
 
 return m
